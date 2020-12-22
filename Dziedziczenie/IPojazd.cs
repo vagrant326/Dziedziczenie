13a3a28eb02a98cd626112c9831312b32318a7ca
vagrant326 <vagrant326@gmail.com>
2020-12-22 19:25:13 +0100

    Init

---
+++ Dziedziczenie/IPojazd.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dziedziczenie
+{
+    /// <summary>
+    /// Interfejs który należy zaimplementować
+    /// </summary>
+    public interface IPojazd
+    {
+        void Jedz();
+        void Jedz(int dystans);
+    }
+}
