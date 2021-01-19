5495acdb2e712067eb543db00ebae19027c126f6
mikeyMike6 <71586476+mikeyMike6@users.noreply.github.com>
2021-01-19 20:09:14 +0100

    t

---
+++ MS055950/V8.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Dziedziczenie;
+
+namespace MS055950
+{
+    public class V8 : Silnik
+    {
+        public V8(int moc, double spalanie) : base(moc, spalanie, "brum ")
+        {
+
+        }
+        public new void Dzialaj()
+        {
+            Console.WriteLine(Dzwiek);
+        }
+    }
+}
