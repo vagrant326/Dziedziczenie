8cc1d2402201b659f5edc5f3add97f2bbd6acecd
Dominovsky <hejdominowy@outlook.com>
2021-01-23 17:34:43 +0100

    Init

---
+++ DK055906/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DK055906
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
