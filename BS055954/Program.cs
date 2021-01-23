e804b3b89f91d5c0be8af6c7dc34e0b356b79e7f
Atelir <atelir911@gmail.com>
2021-01-23 09:07:58 +0100

    Brak pomysłu na coś ambitniejszego

---
+++ BS055954/Program.cs
@@ -0,0 +1,46 @@
+﻿using Dziedziczenie;
+using System.Collections.Generic;
+using System;
+
+namespace BS055954
+{
+    class Program
+    {
+
+        public interface IPojazd {
+
+
+
+        }
+
+        public class KlasaTestowa : Silnik, IPojazd
+        {
+            public Silnik Silnik { get; set; }
+
+            public void Jedz() => Jedz(100);
+
+
+            public void Jedz(int dystans)
+            {
+                for (int i = 0; i < dystans; i++)
+                {
+                    Silnik.Dzialaj();
+                }
+            }
+
+            public KlasaTestowa(int moc, double spalanie, string dzwiek) : base(moc, spalanie, dzwiek)
+            {
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+           
+
+
+
+        }
+    }
+}
