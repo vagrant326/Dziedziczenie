50b97d61b2ef36b702bf8f4f4cd4adc39947785d
Daniel Huber <thyvener@gmail.com>
2021-01-30 16:23:12 +0100

    zad8_v2

--- DH055847/Fighter.cs
+++ DH055847/Fighter.cs
@@ -10,7 +10,7 @@ namespace DH055847
 
         public void Jedz()
         {
-            Jedz(2137);
+            Jedz(9000);
             Console.Write($"Pew pew pew");
         }
         public void Jedz(int dystans)
@@ -28,23 +28,6 @@ namespace DH055847
 
     }
 
-    class KuatDriveYards : Silnik
-    {
+  
 
-        public KuatDriveYards(int moc, double spalanie):base( moc, spalanie, "eeeeeeEEEEEEEEUUUUUUUOOOOOOOOOOooooooooooooo")
-        {
-
-        }
-
-    }
-
-        class Program
-    {
-        static void Main(string[] args)
-        {
-            Fighter tieInterceptor = new Fighter();
-            tieInterceptor.Jedz();
-            
-        }
-    }
 }
