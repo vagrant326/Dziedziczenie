96ead7d551c71ee1598e371f6067f202b9a076a4
Saltator <sebastian.krzus@gmail.com>
2021-01-31 13:42:10 +0100

    Setup

---
+++ SK055913/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SK055913
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
