cfcba0686e3b4ab81f06755aaeefe79b1e1195a3
Marcin Banaś <mb056625@student.ath.edu.pl>
2021-02-03 11:20:48 +0100

    v13

---
+++ MB056625/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MB056625
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SilnikSpalinowy p = new SilnikSpalinowy();
+            Auto a = new Auto(p);
+            a.Jedz();
+        }
+    }
+}
