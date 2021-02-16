179410f7968b991723cba08143819ff126bfcbcd
mikeUwU <salto122@outlook.com>
2021-02-16 01:08:37 -0800

    Update

---
+++ MK055917/SilnikSpalinowy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MK055917
+{
+    public class SilnikSpalinowy : Dziedziczenie.Silnik
+    {
+        public SilnikSpalinowy(int moc, double spalanie) : base(moc, spalanie, "kle")
+        {
+
+        }
+    }
+}
