6045226750ec4b26397e41a93bd46044254a1dc4
Bartłomiej Fijak <bf043557@student.ath.edu.pl>
2021-02-19 10:49:08 +0100

    lastTry2
    lastTry2

---
+++ BF043557/V8.cs
@@ -0,0 +1,15 @@
+﻿using Dziedziczenie;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BF043557
+{
+    class V8 : Silnik
+    {
+        public V8(int moc, double spalanie, string dzwiek) : base(moc, spalanie, "Wrrrrrrrrrrrrrrrrtsssswrrrrrrrrrrrtssssswrrrrrrrrrrrtsss")
+        {
+        }
+
+    }
+}
